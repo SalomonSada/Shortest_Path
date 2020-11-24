b7d5aa80bb8b7484b05063330ae973a9c4df8d61
Salomon Sada <sajosaca@gmail.com>
2020-11-24 18:51:25 -0300

    uploading, beta -v

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PathFindingVisualizer from './pathFindingVisualizer/PathFindingVisualizer';
+
+function App() {
+  return (
+    <div>
+      <PathFindingVisualizer></PathFindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
